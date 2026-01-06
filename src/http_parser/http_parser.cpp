bc415abbfef423cd23176d67998c4bfd63c094c0
YoungSusanoo <ksovfox@mail.ru>
2026-01-06 21:39:22 +0500

    Complete test version of parser

--- src/http_parser/http_parser.cpp
+++ src/http_parser/http_parser.cpp
@@ -1,7 +1,8 @@
 #include <http_parser/http_parser.hpp>
 
 cs2gsi::HttpParser::HttpParser():
-  parser_ { &HttpParser::parse_method }
+  parser_ { &HttpParser::parse_method },
+  content_length_ {}
 {}
 
 std::tuple< std::size_t, bool > cs2gsi::HttpParser::parse_buffer(std::span< const char > data)
@@ -16,6 +17,32 @@ std::tuple< std::size_t, bool > cs2gsi::HttpParser::parse_buffer(std::span< cons
   return std::tuple< std::size_t, bool > { data.size(), false };
 }
 
+cs2gsi::HttpRequest cs2gsi::HttpParser::get_request()
+{
+  HttpMethod method;
+  if (method_ == "GET")
+  {
+    method = HttpMethod::GET;
+  }
+  else if (method_ == "POST")
+  {
+    method = HttpMethod::POST;
+  }
+  return { method, headers_, body_ };
+}
+
+void cs2gsi::HttpParser::clear()
+{
+  parser_ = &HttpParser::parse_method;
+  headers_.clear();
+  body_.clear();
+  method_.clear();
+  path_.clear();
+  header_name_.clear();
+  header_value_.clear();
+  content_length_ = 0;
+}
+
 bool cs2gsi::HttpParser::parse_method(char c)
 {
   if (c == ' ' && c == '\t')
@@ -27,7 +54,98 @@ bool cs2gsi::HttpParser::parse_method(char c)
   return false;
 }
 
-template < cs2gsi::HttpParser::parse_method_t next>
+bool cs2gsi::HttpParser::parse_path(char c)
+{
+  if (c == ' ' && c == '\t')
+  {
+    parser_ = &cs2gsi::HttpParser::skip_spaces< &cs2gsi::HttpParser::parse_version >;
+    return (this->*parser_)(c);
+  }
+  path_.push_back(c);
+  return false;
+}
+
+bool cs2gsi::HttpParser::parse_version(char c)
+{
+  if (c == '\r')
+  {
+    parser_ = &cs2gsi::HttpParser::parse_newline;
+  }
+  return false;
+}
+
+bool cs2gsi::HttpParser::parse_header_name(char c)
+{
+  if (c == '\r')
+  {
+    parser_ = &cs2gsi::HttpParser::parse_newline;
+  }
+  else if (c == ':')
+  {
+    parser_ = &cs2gsi::HttpParser::skip_spaces< &cs2gsi::HttpParser::parse_header_value >;
+  }
+  else
+  {
+    header_value_.push_back(c);
+    return (this->*parser_)(c);
+  }
+  return false;
+}
+
+bool cs2gsi::HttpParser::parse_header_value(char c)
+{
+  if (c == '\r')
+  {
+    parser_ = &cs2gsi::HttpParser::parse_newline;
+  }
+  else
+  {
+    header_value_.push_back(c);
+    return (this->*parser_)(c);
+  }
+  return false;
+}
+
+bool cs2gsi::HttpParser::parse_body(char c)
+{
+  if (body_.length() == content_length_)
+  {
+    return finalize_parse();
+  }
+  body_.push_back(c);
+  return false;
+}
+
+bool cs2gsi::HttpParser::finalize_parse()
+{
+  if (headers_.empty())
+  {
+    return false;
+  }
+  if (header_name_.empty())
+  {
+    if (headers_.contains("Content-Length"))
+    {
+      content_length_ = std::stoull(headers_["Content-Length"]);
+      parser_ = &cs2gsi::HttpParser::parse_body;
+    }
+    else
+    {
+      return true;
+    }
+  }
+  else
+  {
+    headers_.emplace(header_name_, header_value_);
+    header_name_.clear();
+    header_value_.clear();
+    parser_ = &cs2gsi::HttpParser::parse_header_name;
+  }
+  return false;
+  // sanitize returning
+}
+
+template < cs2gsi::HttpParser::parse_method_t next >
 bool cs2gsi::HttpParser::skip_spaces(char c)
 {
   if (c != ' ' && c != '\t')
@@ -35,5 +153,12 @@ bool cs2gsi::HttpParser::skip_spaces(char c)
     parser_ = next;
     return (this->*next)(c);
   }
+  return false;
+}
+
+bool cs2gsi::HttpParser::parse_newline(char c)
+{
+  if (c == '\n')
+  {}
   return false;
 }
--- src/http_parser/http_parser.hpp
+++ src/http_parser/http_parser.hpp
@@ -26,31 +26,33 @@ namespace cs2gsi
     HttpParser();
 
     std::tuple< std::size_t, bool > parse_buffer(std::span< const char > data);
-    bool finalize_parse();
     HttpRequest get_request();
+    void clear();
 
   private:
     using parse_method_t = bool (HttpParser::*)(char);
 
     parse_method_t parser_;
-    HttpRequest request_;
+
+    std::unordered_map< std::string, std::string > headers_;
+    std::string body_;
+
     std::string method_;
     std::string path_;
     std::string header_name_;
     std::string header_value_;
-    std::string body_;
+    std::size_t content_length_;
 
     template < parse_method_t next >
     bool skip_spaces(char c);
-    template < parse_method_t next >
     bool parse_newline(char c);
     bool parse_method(char c);
     bool parse_path(char c);
     bool parse_version(char c);
     bool parse_header_name(char c);
-    bool parse_header_separator(char c);
     bool parse_header_value(char c);
     bool parse_body(char c);
+    bool finalize_parse();
   };
 }
 
